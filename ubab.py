902de8a54726b47b9b76c5758dc62629b30452bb
ubab2 <65265996+ubab2@users.noreply.github.com>
2020-09-11 08:43:30 +0900

    Update ubab.py

--- ubab.py
+++ ubab.py
@@ -15,29 +15,29 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("월요일 시간표"):
-        await message.channel.send("체육 음악 정보 영어 지구과학 국어 창체")
+        await message.channel.send("사회 영어(회화) 국어 화학 지구과학 실험 창체")
     if message.content.startswith("!월시"):
-        await message.channel.send("체육 음악 정보 영어 지구과학 국어 창체")
+        await message.channel.send("체사회 영어(회화) 국어 화학 지구과학 실험 창체")
 
     if message.content.startswith("!화요일 시간표"):
-        await message.channel.send("실험 사회 정보2 수학 한국사 영어")
+        await message.channel.send("수학 국어 한국사 체육 음악 정보 정보")
     if message.content.startswith("!화시"):
-        await message.channel.send("실험 사회 정보2 수학 한국사 영어")
+        await message.channel.send("수학 국어 한국사 체육 음악 정보 정보")
 
     if message.content.startswith("!수요일 시간표"):
-        await message.channel.send("국어 음악 영어 수학 창체")
+        await message.channel.send("국어 영어 수학 사회 창체")
     if message.content.startswith("!수시"):
-        await message.channel.send("국어 음악 영어 수학 창체")
+        await message.channel.send("국어 영어 수학 사회 창체")
 
     if message.content.startswith("!목요일 시간표"):
-        await message.channel.send("물리 국어 영어 한국사 수학 체육 사회")
+        await message.channel.send("물리 체욱 한국사 생물 음악 수학 영어")
     if message.content.startswith("!목시"):
-        await message.channel.send("물리 국어 영어 한국사 수학 체육 사회")
+        await message.channel.send("물리 체욱 한국사 생물 음악 수학 영어")
 
     if message.content.startswith("!금요일 시간표"):
-        await message.channel.send("수학 한국사 화학 사회 생물 진로 국어")
+        await message.channel.send("정보 영어 진로 수학 국사 사회 국어")
     if message.content.startswith("!금시"):
-        await message.channel.send("수학 한국사 화학 사회 생물 진로 국어")
+        await message.channel.send("정보 영어 진로 수학 국사 사회 국어")
 
    
 
